a715e2a69e646dbafdb8ddb4b43b11519885452d
TaherTadpatri <tahertadpatri@gmail.com>
2024-03-23 10:31:55 +0530

    first commit

---
+++ pratice/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+        <div className="Navbar">
+          <ul className='container'>
+            <li className='one'>home
+            <p>Lorem ipsum dolor sit amet consectetur 
+              adipisicing elit. Nam, reiciendis.</p></li>
+            <li className='two'>About</li>  
+         
+
+          </ul>
+        </div>
+    </>
+  )
+}
+
+export default App
